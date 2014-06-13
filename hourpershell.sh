79a4106f0659e6506ed19d86412264c2309d1615
hacktea8 <admin@hacktea8.com>
2014-06-13 15:24:07 +0800

    update

--- hourpershell.sh
+++ hourpershell.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
-/usr/local/php/bin/php  /var/www/html/cront/comics/hhcomic/sixhourexec.php &
+sleep 1m
+/usr/local/php/bin/php  /var/www/html/emules/cront/task/sethitsnum.php >/dev/null &
+sleep 1m
+/usr/local/php/bin/php  /var/www/html/btvideo/cront/task/sethitsnum.php >/dev/null &
+sleep 1m
+/usr/local/php/bin/php  /var/www/html/joker/cront/task/sethitsnum.php >/dev/null &
+sleep 1m
+/usr/local/php/bin/php  /var/www/html/btvideo/cront/grab/vvtor/grab.php >/dev/null &
+#/usr/local/php/bin/php  /var/www/html/cront/comics/hhcomic/sixhourexec.php &
 sleep 1m
 /usr/local/php/bin/php  /var/www/html/cront/emule/ed2kers/sixhourexec.php &
-sleep 1m
-/usr/local/php/bin/php  /var/www/html/emules/cront/task/sethitsnum.php & >/dev/null
-sleep 1m
-/usr/local/php/bin/php  /var/www/html/btvideo/cront/task/sethitsnum.php & >/dev/null
 
