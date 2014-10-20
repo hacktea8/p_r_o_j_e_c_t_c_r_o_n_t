5d2d729ad0ae9e22c697153742b0e8e52c6278ca
jason <jason@emubt.com>
2014-10-20 09:47:41 +0800

    update

--- hourpershell.sh
+++ hourpershell.sh
@@ -8,10 +8,10 @@ sleep 1m
 sleep 1m
 /usr/local/php/bin/php  /var/www/html/qvodp2p/cront/task/sethitsnum.php >/dev/null &
 sleep 1m
-/usr/local/php/bin/php  /var/www/html/btvideo/cront/grab/vvtor/grab.php >/dev/null &
+/usr/local/php/bin/php  /var/www/html/btvideo/cront/grab/vvtor/grab.php >/dev/null 
 #sleep 1m
 #/usr/local/php/bin/php  /var/www/html/qvodp2p/cront/grab/tutuzx/grab.php >/dev/null &
 #/usr/local/php/bin/php  /var/www/html/cront/comics/hhcomic/sixhourexec.php &
 sleep 1m
-/usr/local/php/bin/php  /var/www/html/cront/emule/ed2kers/sixhourexec.php &
+/usr/local/php/bin/php  /var/www/html/cront/emule/ed2kers/sixhourexec.php 
 
