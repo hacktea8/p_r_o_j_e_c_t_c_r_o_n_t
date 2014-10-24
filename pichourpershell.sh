80bab24a0a60d5a5833de45489f7e94438ca2413
jason <jason@emubt.com>
2014-10-24 22:30:40 +0800

    update

--- pichourpershell.sh
+++ pichourpershell.sh
@@ -5,5 +5,5 @@ sleep 1m
 /usr/local/php/bin/php  /var/www/html/qvodp2p/cront/grab/tutuzx/grab.php >/dev/null
 #/usr/local/php/bin/php  /var/www/html/cront/comics/hhcomic/sixhourexec.php &
 sleep 1m
-/usr/local/php/bin/php  /var/www/html/cront/emule/ed2kers/sixhourexec.php 
+/usr/local/php/bin/php  /var/www/html/cront/emule/ed2kers/sixhourexec.php >/dev/null
 
